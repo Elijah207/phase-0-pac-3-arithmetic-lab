66a6556c40db1bd55b686f0094440eeb33a16bea
elijah207 <elijahvanfuller@gmail.com>
2022-09-30 20:57:43 -0700

    commit tree 6150b9917e8f33a0014e0aeee454366ee3587ed2

---
+++ index.js
@@ -0,0 +1,25 @@
+function add(a,b) {
+    return a+b
+    // we'll fill this in shortly
+  }
+  function subtract(a,b) {
+    return a-b
+    // we'll fill this in shortly
+  }
+  function divide(a,b) {
+    return a/b
+    // we'll fill this in shortly
+  }
+  function multiply(a,b) {
+    return a* b
+    // we'll fill this in shortly
+  }
+  function increment(n) {  
+    return n++  
+    }
+    function increment(n) {
+        return ++n
+        }
+        function decrement(n) {
+            return --n
+            }
